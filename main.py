8990b8f59f204e904f4bc2108b31ffa4cc9165ea
Estefania <aida.estefania.sosa@gmail.com>
2025-06-18 12:02:27 +0200

    obtains data from a small sample of urls - links without "project" properties

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from links_scrapping import listingScrapping
 def main():
     listings = listingScrapping()
     listings.call_driver()
-    listings.save_to_csv("listing_links-7-9-prov.csv")
+    listings.save_to_csv("listing_links.csv")
 
 
 if __name__ == "__main__":
